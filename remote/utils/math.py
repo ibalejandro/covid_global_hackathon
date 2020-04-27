7be28d3d62174edaba56f61026424e965708d959
Juan Manuel Gutierrez <jgutierrezh0@gmail.com>
2020-04-27 17:18:52 -0500

    Changing convolutional from OOP to functional

--- remote/utils/math.py
+++ remote/utils/math.py
@@ -7,6 +7,6 @@ def conv_2d(frames, kernel):
     kh, kw = kernel.shape
     frames = frames.reshape((n, h, w, 1)).astype(np.float32)
     kernel = kernel.reshape(kh, kw, 1, 1)
-    conv_op = tf.keras.layers.Conv2D(filters=1, kernel_size=(kh, kw), use_bias=False, input_shape=(h, w, 1),
-                                     kernel_initializer=tf.keras.initializers.Constant(kernel))
-    return conv_op(frames).numpy().reshape((n, h - kh + 1, w - kw + 1))
+    convs = tf.keras.backend.conv2d(frames, kernel=kernel).numpy().reshape((n, h - kh + 1, w - kw + 1))
+    tf.keras.backend.clear_session()
+    return convs
